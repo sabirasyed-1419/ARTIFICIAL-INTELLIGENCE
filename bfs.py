12d12323cd01e1ee8d5267fecbd6b126c95eb103
sabirasyed-1419 <syedsabira3044.sse@saveetha.com>
2025-05-03 12:29:29 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+# Sample graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            queue.extend(neighbor for neighbor in graph[node] if neighbor not in visited)
+
+# Run BFS
+bfs(graph, 'A')
