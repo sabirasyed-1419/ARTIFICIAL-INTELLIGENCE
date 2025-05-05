4b235d0a825c3f5b85d07a97e73ade17c800924b
sabirasyed-1419 <syedsabira3044.sse@saveetha.com>
2025-05-05 15:27:00 +0530

    Add files via upload

---
+++ missionary and cannibals.py
@@ -0,0 +1,30 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/syeds/AppData/Local/Programs/Python/Python313/Missionary and Cannibal .py
+(3, 3, 1)
+(3, 1, 0)
+(3, 2, 1)
+(3, 0, 0)
+(3, 1, 1)
+(1, 1, 0)
+(2, 2, 1)
+(0, 2, 0)
+(0, 3, 1)
+(0, 1, 0)
+(1, 1, 1)
+(0, 0, 0)
+>>> 
+= RESTART: C:/Users/syeds/AppData/Local/Programs/Python/Python313/Missionary and Cannibal .py
+(3, 3, 1)
+(3, 1, 0)
+(3, 2, 1)
+(3, 0, 0)
+(3, 1, 1)
+(1, 1, 0)
+(2, 2, 1)
+(0, 2, 0)
+(0, 3, 1)
+(0, 1, 0)
+(1, 1, 1)
+(0, 0, 0)
